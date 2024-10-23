2dbe4835108cd8972f38f7949bbd3722745c1213
Kauã Lorenzi <157428596+Kilmo0@users.noreply.github.com>
2024-10-23 15:36:47 -0300

    Exercicio 2.3

---
+++ Exercicios Livro de Python/Parte 1/Capitulo 2 - Variaveis/Exercicio 2.3.py
@@ -0,0 +1,7 @@
+#2.3 – Mensagem pessoal: Armazene o nome de uma pessoa em uma variável e
+#apresente uma mensagem a essa pessoa. Sua mensagem deve ser simples, como
+
+nome = 'Kauã'
+mensagem = 'olá {} como você está?'.format(nome)
+print(mensagem)
+
