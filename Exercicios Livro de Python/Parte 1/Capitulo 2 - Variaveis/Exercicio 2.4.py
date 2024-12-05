a839ecd9d16c8e97760ec1fd4e9a20f47a06068a
Kauã Lorenzi <157428596+Kilmo0@users.noreply.github.com>
2024-12-05 16:02:28 -0300

    Exercicio 2.4

---
+++ Exercicios Livro de Python/Parte 1/Capitulo 2 - Variaveis/Exercicio 2.4.py
@@ -0,0 +1,13 @@
+#letra maisculas e minusculas em nomes
+#Armazene nome de uma pessoa em uma variavel e apresente o nome dessa pessoa em letras maisculas minuscula e somente a primeira pessoa minuscula
+
+print('bom dia tudo certo? poderia me dizer seu nome?')
+nome = input()
+
+#print('o seu nome  é kaua')
+#print('o seu em maisculo é {}'.format(nome.upper()))
+#print('seu nome em minusculo é {}'.format(nome.lower()))
+#print('e seu nome somente com a primeira letra maicula é {}'.format(nome.title()))
+
+# também poderiamos usar o comando
+print('a variação maiscula minuscula e somente com a primeira letra maiscula do seu nome é: {}, {}, {},'.format(nome.upper(), nome.lower(), nome.title()))
