e3c262ea542d584441fd2ff3faba394c4dcacd01
Kauã Lorenzi <157428596+Kilmo0@users.noreply.github.com>
2024-12-09 14:56:48 -0300

    Exercicio 2.5

--- Exercicios Livro de Python/Parte 1/Capitulo 2 - Variaveis/Exercicio 2.4.py
+++ Exercicios Livro de Python/Parte 1/Capitulo 2 - Variaveis/Exercicio 2.4.py
@@ -1,13 +1,15 @@
 #letra maisculas e minusculas em nomes
 #Armazene nome de uma pessoa em uma variavel e apresente o nome dessa pessoa em letras maisculas minuscula e somente a primeira pessoa minuscula
 
+#introdução/complementos
 print('bom dia tudo certo? poderia me dizer seu nome?')
 nome = input()
 
-#print('o seu nome  é kaua')
-#print('o seu em maisculo é {}'.format(nome.upper()))
-#print('seu nome em minusculo é {}'.format(nome.lower()))
-#print('e seu nome somente com a primeira letra maicula é {}'.format(nome.title()))
+#utilizando o codigo
+print('o seu nome  é kaua')
+print('o seu em maisculo é {}'.format(nome.upper()))
+print('seu nome em minusculo é {}'.format(nome.lower()))
+print('e seu nome somente com a primeira letra maicula é {}'.format(nome.title()))
 
-# também poderiamos usar o comando
-print('a variação maiscula minuscula e somente com a primeira letra maiscula do seu nome é: {}, {}, {},'.format(nome.upper(), nome.lower(), nome.title()))
+#também poderiamos usar o comando
+#print('a variação maiscula minuscula e somente com a primeira letra maiscula do seu nome é: {}, {}, {},'.format(nome.upper(), nome.lower(), nome.title()))
---
+++ Exercicios Livro de Python/Parte 1/Capitulo 2 - Variaveis/Exercicio 2.5.py
@@ -0,0 +1,5 @@
+#citação famosa
+#encontre uma citação famosa de uma pessoa famosa que você admire exiba a citação e o nome do altor exemplo: Albert Einstein certa vez disse: “Uma pessoa que nunca cometeu
+#um erro jamais tentou nada novo.”
+
+print('Elon musk uma vez disse: "Quando algo é importante o suficiente, você realiza mesmo que as chances não estejam ao seu favor"')
