225f4dc66e520ae741ea20625750900145775e60
Kauã Lorenzi <157428596+Kilmo0@users.noreply.github.com>
2024-12-12 16:22:14 -0300

    Exercicio 3.4 concluido

---
+++ Exercicios Livro de Python/Parte 1/Capitulo 3 - Listas/Exercicio 3.4.py
@@ -0,0 +1,11 @@
+""""
+3.4 – Lista de convidados: Se pudesse convidar alguém, vivo ou morto, para o
+jantar, quem você convidaria? Crie uma lista que inclua pelo menos três pessoas
+que você gostaria de convidar para jantar. Em seguida, utilize sua lista para exibir
+uma mensagem para cada pessoa, convidando-a para jantar
+"""
+
+convidados = ['Jesus', 'Lucifer', 'leonardo da Vince', 'albert einsten', 'Amanda Janke']
+
+for convidado in convidados:
+    print('olá {} espero que esteja tudo bem com você!\nvenho lhe convidar para um jantar para debater fatos Contamos muito sua presença \n\nAss: Kauã Lorenzi'.format(convidado.title()))
--- Teste.py
+++ Teste.py
@@ -1,5 +1,15 @@
-Comida = ['Arroz', 'Feijão', 'carne']
+cripto = []
 
-Comida.append('Batata')
+cripto.append('bitcoin')
+cripto.append('ethereum')
+cripto.append('cardano')
 
-print(Comida)
+cripto.insert(0, "XRP")
+
+nocripto = 'ethereum'
+cripto.remove(nocripto)
+
+for criptos in cripto:
+    print(criptos)
+
+print("{} não é uma criptomoeda".format(nocripto.title()))
