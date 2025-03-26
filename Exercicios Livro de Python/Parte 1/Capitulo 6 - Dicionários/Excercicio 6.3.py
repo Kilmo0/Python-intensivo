e4565a9a0cb153d232afbecdb2a4479aac8ea85a
Kauã Lorenzi <157428596+Kilmo0@users.noreply.github.com>
2025-03-26 09:49:07 -0300

    Excercicio 6.3

---
+++ Exercicios Livro de Python/Parte 1/Capitulo 6 - Dicionários/Excercicio 6.3.py
@@ -0,0 +1,8 @@
+glossarios = {
+    'lista': 'lista é um conjunto de valores',
+    'variavel': 'conjunto de valores pré-definidos',
+    'laço': 'repitação de valores'
+}
+
+for glossario in glossarios:
+    print('{} significa:\n{}'.format(glossario, glossarios[glossario]))
--- Teste.py
+++ Teste.py
@@ -1,13 +1,5 @@
-#sistema de login
-
-users = ['Amanda', 'Kauã', 'Allan']
-newusers = ['Amanda', 'Brian', 'Ana']
-
-lowusers = (lowusersa.lower() for lowusersa in users)
-newlowusers = (newlowusersa.lower() for newlowusersa in newusers)
-
-for newuser in newlowusers:
-    if not newuser in lowusers:
-        print('Seja bem vindo {}'.format(newuser))
-    else:
-        print('você já está cadastrado {}'.format(newuser))
+favlinguagem = {
+    'Kauã': 'java',
+    'Allan': 'php',
+    'Brian': 'Rust',
+}
