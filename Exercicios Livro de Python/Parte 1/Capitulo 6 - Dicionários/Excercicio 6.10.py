72dcf79de2e7941bba8d2bd0f8105cb273ea350d
Kilmo <157428596+Kilmo0@users.noreply.github.com>
2025-03-31 20:30:15 -0300

    Excercicio 6.10

---
+++ Exercicios Livro de Python/Parte 1/Capitulo 6 - Dicionários/Excercicio 6.10.py
@@ -0,0 +1,9 @@
+Usuários = {
+  'kauã Lorenzi': ['1', '9']
+   
+  
+}
+
+for nome, numeros in Usuários.items():
+    numerostr = ', '.join(numeros)
+    print(f'meu nome é {nome}, e gosto dos numeros: {numerostr}')
---
+++ Exercicios Livro de Python/Parte 1/Capitulo 6 - Dicionários/Excercicio 6.9.py
@@ -0,0 +1,8 @@
+lugares = {
+    'Allan': 'Havai',
+    'Brian': 'Pizzaria',
+    'Amanda': 'Figueira Sushi',
+}
+
+for nomes, lugar in lugares.items():
+    print(f'o lugar favorito do(a) {nomes}, é {lugar}')
