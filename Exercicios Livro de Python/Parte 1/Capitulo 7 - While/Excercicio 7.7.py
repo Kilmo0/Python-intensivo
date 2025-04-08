b75aec53bf62ab27e707823f4975d3e7c1f56280
Kauã Lorenzi <157428596+Kilmo0@users.noreply.github.com>
2025-04-08 14:51:36 -0300

    7.7

---
+++ Exercicios Livro de Python/Parte 1/Capitulo 7 - While/Excercicio 7.7.py
@@ -0,0 +1,6 @@
+num1 = input()
+num1 = int(num1)
+
+while num1 >= 1:
+    num1 = num1 + 1
+    print(num1)
--- Teste.py
+++ Teste.py
@@ -1,7 +1,9 @@
-num1 = 0
-
-while num1 <= 10:
-    num1 = num1 + 1
-    if num1 % 2 == 0:
-        continue
-    print(num1)
+if 
+else 
+elif
+not 
+in 
+!= 
+>= 
+<=
+== 
